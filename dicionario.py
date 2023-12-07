efd86ec76ff694c7404f16f8faa4890192aab45f
isagalhardo <142837654+isagalhardo@users.noreply.github.com>
2023-12-07 16:49:25 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,32 @@
+""""
+cliente = {
+    "nome" : "isabella",
+    "idade " : "17anos",
+    "email" : "isabellagalhardo925@gmail.com"
+}
+
+#print(type(cliente))
+#print(cliente)
+print (cliente["nome"])
+cliente["nome"] = "joao" # troquei o nome 
+print(cliente["nome"])
+cliente.update({"cpf" : "123123123"})
+print(cliente)
+"""
+alunos =  {
+    
+        "aluno1": {
+    
+      "nome" : "mateus",
+      "email" : "mateus@gmail.com",
+      "fone" : ["129833213232","12987676767"],
+      "cursos" : ["tecnico em internet", "Python"],
+    },
+        "aluno2":  {
+      "nome" : "mateus",
+      "email" : "mateus@gmail.com",
+      "fone" : ["129833213232","12987676767"],
+      "cursos" : ["tecnico em internet", "Python"],
+    }
+}
+print(type[alunos])
